53a31adb3b5ec2f77a2520eb42a6be518a191540
Alexander Kurganov <me@akurganow.ru>
2017-06-20 17:52:57 +0300

    Update webpack

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,32 +8,38 @@ exports.modifyWebpackConfig = function(config, env) {
   }
 
   config.removeLoader('less')
+  config.removeLoader('lessModules')
   config.removeLoader('sass')
+  config.removeLoader('sassModules')
 
   if (env === 'develop') {
     config.removeLoader('css')
+    config.removeLoader('cssModules')
+    config.loader('cssModules', {
+      test: /\.module.css$/,
+      loaders: ['style', 'css?modules&localIdentName=[name]_[local]__[hash:base64:8]', 'postcss'],
+    })
     config.loader('css', {
       test: /\.css$/,
+      exclude: /\.module\.css$/,
       loaders: ['style', 'css', 'postcss'],
-    })
-    config.loader('module.css', {
-      test: /\.module.css$/,
-      loaders: ['style', 'css?modules&localIdentName=[name]_[local]__[hash:base64:5]', 'postcss'],
     })
   }
   else {
     config.removeLoader('css')
-    config.loader('css', {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract(['css', 'postcss']),
-    })
-    config.loader('module.css', {
+    config.removeLoader('cssModules')
+    config.loader('cssModules', {
       test: /\.module.css$/,
       loader: ExtractTextPlugin.extract(['css?modules&localIdentName=[hash:base64:8]', 'postcss']),
     })
+    config.loader('css', {
+      test: /\.css$/,
+      exclude: /\.module\.css$/,
+      loader: ExtractTextPlugin.extract(['css', 'postcss']),
+    })
   }
 
-  config.merge({plugins: [new ExtractTextPlugin('styles.css')]})
+  config.plugin('extract-css', ExtractTextPlugin, ['styles.css', { allChunks: true }])
 
   return config
 }
