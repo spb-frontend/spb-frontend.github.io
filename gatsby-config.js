e015c8f38272866b873e026102bb287cf5e01a6f
Alexander Kurganov <me@akurganow.ru>
2017-07-16 12:04:22 +0300

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,84 @@
 module.exports = {
   siteMetadata: {
-    title: `SPB Frontend official site`,
+    title: 'SPB Frontend',
+    description: 'SPB Frontend official site',
+    siteUrl: 'https://spb-frontend.ru',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'podcast',
+        path: `${__dirname}/podcast`
+      }
+    },
+    'gatsby-transformer-remark',
+    'gatsby-transformer-json',
+    {
+      resolve: 'plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: {
+                    fileAbsolutePath: { regex: "/podcast/" }
+                  }
+                ) {
+                  edges {
+                    node {
+                      html
+                      frontmatter {
+                        link
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/podcast.xml'
+          }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'SPB Frontend official site',
+        short_name: 'Akurganow',
+        start_url: '/',
+        background_color: '#fff',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/favicons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/favicons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      }
+    },
+    'gatsby-plugin-react-helmet'
+  ],
 }
