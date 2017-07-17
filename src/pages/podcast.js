c5cf482cbd90599193820e45b3f1268d6a225145
Lekzd <lekzd@mail.ru>
2017-07-17 06:09:30 +0300

    Podcast Component functions combined to one class

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -1,53 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 
 import styles from './../css/podcast.module.css'
 import {defaultHelmetMeta} from '../layouts/index'
 
-const getPost = ({ node }) => {
-  const {title, link, date} = node.frontmatter
+export default class Podcast  extends Component {
 
-  return (
-    <div className={styles.post}>
-      <header className={styles.header}>
-        <h3 className={styles.header_title}>{title}</h3>
-        <date className={styles.header_date}>{date}</date>
-      </header>
-      <audio
-        className={styles.audio}
-        controls='controls'
-        preload='none'
-        src={link} />
-      <footer
-        className={styles.footer}
-        dangerouslySetInnerHTML={{ __html: node.html }} />
-    </div>
-  )
+  constructor(props) {
+    super(props)
+
+    const posts = props.data.allMarkdownRemark.edges
+    this.postList = posts.map(this.getPost.bind(this))
+  }
+
+  getPost({ node }, index) {
+    const {title, link, date} = node.frontmatter
+
+    return (
+      <div
+        key={index}
+        className={styles.post}>
+        <header className={styles.header}>
+          <h3 className={styles.header_title}>{title}</h3>
+          <date className={styles.header_date}>{date}</date>
+        </header>
+        <audio
+          className={styles.audio}
+          controls='controls'
+          preload='none'
+          src={link} />
+        <footer
+          className={styles.footer}
+          dangerouslySetInnerHTML={{ __html: node.html }} />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className={styles.title}>
+          🍻 Drinkcast 🍻
+        </h2>
+        <div className={styles.posts}>{this.postList}</div>
+
+        <Helmet meta={defaultHelmetMeta}>
+          <title>SPB Frontend. Drinkcast</title>
+          <link
+            rel='alternate'
+            type='application/rss+xml'
+            href='/podcast.xml' />
+        </Helmet>
+      </div>
+    )
+  }
 }
-
-const Podcast = ({data}) => {
-  const blogPosts = data.allMarkdownRemark.edges
-  const postList = blogPosts.map(getPost)
-
-  return (
-    <div>
-      <h2 className={styles.title}>
-        🍻 Drinkcast 🍻
-      </h2>
-      <div className={styles.posts}>{postList}</div>
-
-      <Helmet meta={defaultHelmetMeta}>
-        <title>SPB Frontend. Drinkcast</title>
-        <link
-          rel='alternate'
-          type='application/rss+xml'
-          href='/podcast.xml' />
-      </Helmet>
-    </div>
-  )
-}
-
-export default Podcast
 
 export const pageQuery = graphql` query MyQueryName
   {
