5bf11385c647740cf0bd261ba0be3cc3d89d3544
Sergey Lapin <lapanoid@gmail.com>
2017-08-04 14:39:55 +0300

    refactor: format code by "npm run format"

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
-import Helmet from 'react-helmet'
-import Link from 'gatsby-link'
+import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
 
-import styles from './../css/podcast.module.css'
-import {defaultHelmetMeta} from '../layouts/index'
+import styles from './../css/podcast.module.css';
+import { defaultHelmetMeta } from '../layouts/index';
 
-export default class Podcast  extends Component {
-
+export default class Podcast extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    const posts = props.data.allMarkdownRemark.edges
-    this.postList = posts.map(this.getPost.bind(this))
+    const posts = props.data.allMarkdownRemark.edges;
+    this.postList = posts.map(this.getPost.bind(this));
   }
 
   getPost({ node }, index) {
-    const {title, date} = node.frontmatter
-    const postLink = `/podcast/${index + 1}`
+    const { title, date } = node.frontmatter;
+    const postLink = `/podcast/${index + 1}`;
 
     return (
       <div
@@ -30,19 +29,21 @@ export default class Podcast  extends Component {
               {title}
             </Link>
           </h3>
-          <date className={styles.header_date}>{date}</date>
+          <date className={styles.header_date}>
+            {date}
+          </date>
         </header>
       </div>
-    )
+    );
   }
 
   render() {
     return (
       <div>
-        <h2 className={styles.title}>
-          🍻 Drinkcast 🍻
-        </h2>
-        <div className={styles.posts}>{this.postList}</div>
+        <h2 className={styles.title}>🍻 Drinkcast 🍻</h2>
+        <div className={styles.posts}>
+          {this.postList}
+        </div>
 
         <Helmet meta={defaultHelmetMeta}>
           <title>SPB Frontend. Drinkcast</title>
@@ -52,18 +53,16 @@ export default class Podcast  extends Component {
             href='/podcast.xml' />
         </Helmet>
       </div>
-    )
+    );
   }
 }
 
-export const pageQuery = graphql` query MyQueryName
-  {
+export const pageQuery = graphql`
+  query MyQueryName {
     allMarkdownRemark(
-      limit: 1000,
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: {
-        fileAbsolutePath: { regex: "/podcast/" }
-      }
+      limit: 1000
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { fileAbsolutePath: { regex: "/podcast/" } }
     ) {
       edges {
         node {
@@ -77,4 +76,4 @@ export const pageQuery = graphql` query MyQueryName
       }
     }
   }
-`
+`;
