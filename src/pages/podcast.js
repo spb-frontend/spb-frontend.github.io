a3782fa00d7cf5a4c7f4d200c01e96e8dd2b71f5
Sergey Lapin <lapanoid@gmail.com>
2017-08-04 15:35:51 +0300

    refactor: in more reactish way

--- src/pages/podcast.js
+++ src/pages/podcast.js
@@ -5,57 +5,43 @@ import Link from 'gatsby-link'
 import styles from './../css/podcast.module.css'
 import {defaultHelmetMeta} from '../layouts/index'
 
-export default class Podcast extends Component {
-  constructor(props) {
-    super(props)
+const PostLink = ({title, date, to}) =>
+  <div className={styles.post}>
+    <header className={styles.header}>
+      <h3 className={styles.header_title}>
+        <Link
+          className='title'
+          to={to}>
+          {title}
+        </Link>
+      </h3>
+      <date className={styles.header_date}>
+        {date}
+      </date>
+    </header>
+  </div>
 
-    const posts = props.data.allMarkdownRemark.edges
-    this.postList = posts.map(this.getPost.bind(this))
-  }
+export default ({data: {allMarkdownRemark: {edges}}}) =>
+  <div>
+    <h2 className={styles.title}>🍻 Drinkcast 🍻</h2>
+    <div className={styles.posts}>
+      {edges.map(({node: {frontmatter: {title, date}}}, index) =>
+        <PostLink
+          key={index}
+          title={title}
+          date={date}
+          to={`/podcast/${index + 1}`} />,
+      )}
+    </div>
 
-  getPost({node}, index) {
-    const {title, date} = node.frontmatter
-    const postLink = `/podcast/${index + 1}`
-
-    return (
-      <div
-        key={index}
-        className={styles.post}>
-        <header className={styles.header}>
-          <h3 className={styles.header_title}>
-            <Link
-              className='title'
-              to={postLink}>
-              {title}
-            </Link>
-          </h3>
-          <date className={styles.header_date}>
-            {date}
-          </date>
-        </header>
-      </div>
-    )
-  }
-
-  render() {
-    return (
-      <div>
-        <h2 className={styles.title}>🍻 Drinkcast 🍻</h2>
-        <div className={styles.posts}>
-          {this.postList}
-        </div>
-
-        <Helmet meta={defaultHelmetMeta}>
-          <title>SPB Frontend. Drinkcast</title>
-          <link
-            rel='alternate'
-            type='application/rss+xml'
-            href='/podcast.xml' />
-        </Helmet>
-      </div>
-    )
-  }
-}
+    <Helmet meta={defaultHelmetMeta}>
+      <title>SPB Frontend. Drinkcast</title>
+      <link
+        rel='alternate'
+        type='application/rss+xml'
+        href='/podcast.xml' />
+    </Helmet>
+  </div>
 
 export const pageQuery = graphql`
   query MyQueryName {
