ae66e75029fafca9956d29f8d1e36a8fcee21f77
Sergey Lapin <lapanoid@gmail.com>
2017-08-11 10:51:58 +0300

    Revert "refactor: format and remove Object.assign"
    This reverts commit cd8c2f5873d848df20121513df27483fed0a3382.

--- plugins/plugin-feed/gatsby-node.js
+++ plugins/plugin-feed/gatsby-node.js
@@ -1,39 +1,35 @@
-'use strict'
+'use strict';
 require('babel-core/register')
 require('babel-polyfill')
 
 const path = require('path')
 const RSS = require('rss')
-const {defaultOptions, runQuery, writeFile} = require('./internals')
+const { defaultOptions, runQuery, writeFile } = require('./internals')
 
 const publicPath = './public'
 
 // A default function to transform query data into feed entries.
-const serialize = ({site, allMarkdownRemark}) =>
+const serialize = ({ site, allMarkdownRemark }) =>
   allMarkdownRemark.edges.map(edge => {
-    return {
-      ...edge.node.frontmatter,
+    return Object.assign({}, edge.node.frontmatter, {
       description: edge.node.excerpt,
       url: edge.node.frontmatter.link,
       guid: edge.node.frontmatter.link,
       image: edge.node.frontmatter.image,
-      custom_elements: [{'content:encoded': edge.node.html}],
-    }
+      custom_elements: [{ 'content:encoded': edge.node.html }],
+    })
   })
 
-exports.onPostBuild = async ({graphql}, pluginOptions) => {
+exports.onPostBuild = async ({ graphql }, pluginOptions) => {
   delete pluginOptions.plugins
 
   /*
    * Run the site settings query to gather context, then
    * then run the corresponding feed for each query.
    */
-  const {query, setup, feeds, rest} = {
-    ...defaultOptions,
-    ...pluginOptions,
-  }
+  const { query, setup, feeds, rest } = Object.assign({}, defaultOptions, pluginOptions)
   const globals = await runQuery(graphql, query)
-
+  console.log(globals)
   /* TODO: придумать решение получше */
   globals.site.siteMetadata = globals.site.siteMetadata.podcast
 
@@ -44,66 +40,57 @@ exports.onPostBuild = async ({graphql}, pluginOptions) => {
     }
 
     const output = path.join(publicPath, f.output)
-    const ctx = {...globals, ...locals}
-    const feed = new RSS(setup({...rest, ...ctx}))
+    const ctx = Object.assign({}, globals, locals)
+    const feed = new RSS(setup(Object.assign({}, rest, ctx)))
     const items = f.serialize ? f.serialize(ctx) : serialize(ctx)
 
     /* TODO: Вынести настройки в конфиг */
     feed.custom_namespaces = {
-      itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd',
+      itunes: 'http://www.itunes.com/dtds/podcast-1.0.dtd'
     }
     feed.custom_elements = [
       {'itunes:explicit': 'no'},
       {'itunes:subtitle': feed.title},
       {'itunes:author': 'SPB Frontend'},
       {'itunes:summary': feed.description},
-      {
-        'itunes:owner': [
-          {'itunes:name': 'SPB Frontend'},
-          {'itunes:email': 'hi@spn-frontend.ru'},
-        ],
-      },
-      {
-        'itunes:image': {
-          _attr: {
-            href: feed.image_url,
-          },
-        },
-      },
-      {
-        'itunes:category': [
-          {
-            _attr: {
-              text: 'Technology',
-            },
-          },
-        ],
-      },
+      {'itunes:owner': [
+        {'itunes:name': 'SPB Frontend'},
+        {'itunes:email': 'hi@spn-frontend.ru'}
+      ]},
+      {'itunes:image': {
+        _attr: {
+          href: feed.image_url
+        }
+      }},
+      {'itunes:category': [
+        {_attr: {
+          text: 'Technology'
+        }},
+      ]},
     ]
+
+    console.log(feed)
 
     items.forEach(i => {
       i.custom_elements = [
         {'itunes:explicit': i.explicit},
         {'itunes:author': 'SPB Frontend'},
         {'itunes:subtitle': i.title},
-        {
-          'itunes:image': {
-            _attr: {
-              href: i.image,
-            },
-          },
-        },
+        {'itunes:image': {
+          _attr: {
+            href: i.image
+          }
+        }},
         {'itunes:duration': i.duration},
-        {
-          enclosure: {
-            _attr: {
-              type: 'audio/mpeg',
-              url: i.file,
-              length: i.length,
-            },
-          },
-        },
+        {enclosure: {
+          _attr: {
+            type: 'audio/mpeg',
+            url: i.file,
+            length: i.length,
+          }
+        }}
       ]
+
 
       return feed.item(i)
     })
