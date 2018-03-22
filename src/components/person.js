d4131d8545a53095a6f69fb41c7b5f25b6e51174
Alexander Kurganow <me@akurganow.ru>
2018-03-22 22:44:47 +0300

    Sort persons by podcasts

--- src/components/person.js
+++ src/components/person.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
+import moment from 'moment'
 import RSSIcon from '-!svg-react-loader?name=Icon!../../static/rss-symbol.svg'
 import ITunesIcon from '-!svg-react-loader?name=Icon!../../static/itunes.svg'
 import TelegramIcon from '-!svg-react-loader?name=Icon!../../static/telegram.svg'
@@ -8,6 +9,30 @@ import TelegramIcon from '-!svg-react-loader?name=Icon!../../static/telegram.svg
 import styles from './../css/person.module.css'
 import { defaultHelmetMeta } from '../layouts/index'
 import { getPersonId } from './../../utils/person'
+
+function getLastPodcast(array) {
+  const newArray = array.map(podcast => podcast.number)
+  const last = Math.max.apply(null, newArray)
+
+  return last
+}
+
+function sortPersons(array) {
+  const newArray = [...array]
+    .sort((prev, next) => {
+      if (prev.node.podcasts.length !== next.node.podcasts.length) {
+        return next.node.podcasts.length - prev.node.podcasts.length
+      }
+      else {
+        const nextLastPodcast = getLastPodcast(next.node.podcasts)
+        const prevLastPodcast = getLastPodcast(prev.node.podcasts)
+
+        return nextLastPodcast - prevLastPodcast
+      }
+    })
+
+  return newArray
+}
 
 const Person = ({ node: { name, lastname, photo } }) => {
   const personId = getPersonId(name, lastname)
@@ -32,10 +57,12 @@ const Person = ({ node: { name, lastname, photo } }) => {
 
 export default props => {
   const { pathContext: { data: person } } = props
+  const sortedPersons = sortPersons(person)
+
   return (
     <div>
       <div className={styles.list}>
-        {person.map(({ node }, index) => {
+        {sortedPersons.map(({ node }, index) => {
           return <Person
             key={index}
             node={node} />
