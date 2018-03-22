950151dcd2fb1721f3a2b4b74a93f86a12acb508
Alexander Kurganow <me@akurganow.ru>
2018-03-22 22:23:31 +0300

    Fix code style

--- src/components/person.js
+++ src/components/person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import RSSIcon from '-!svg-react-loader?name=Icon!../../static/rss-symbol.svg'
@@ -6,32 +6,36 @@ import ITunesIcon from '-!svg-react-loader?name=Icon!../../static/itunes.svg'
 import TelegramIcon from '-!svg-react-loader?name=Icon!../../static/telegram.svg'
 
 import styles from './../css/person.module.css'
-import {defaultHelmetMeta} from '../layouts/index'
-import {getPersonId} from './../../utils/person'
+import { defaultHelmetMeta } from '../layouts/index'
+import { getPersonId } from './../../utils/person'
 
-const Person = ({node: {name, lastname, photo}}) => (
-  <div className={styles.item}>
-    <div className={styles.photo}>
-      <Link to={`/person/${getPersonId(name, lastname)}`}>
-        {photo ? (
-          <img src={`https:${photo.file.url}?fit=thumb&h=100&w=100`} />
-        ) : (
-          <img src='/Person-placeholder.jpg' />
-        )}
+const Person = ({ node: { name, lastname, photo } }) => {
+  const personId = getPersonId(name, lastname)
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.photo}>
+        <Link to={`/person/${personId}`}>
+          {photo ? (
+            <img src={`https:${photo.file.url}?fit=thumb&h=100&w=100`} />
+          ) : (
+            <img src='/Person-placeholder.jpg' />
+          )}
+        </Link>
+      </div>
+      <Link to={`/person/${personId}`}>
+        {name} {lastname}
       </Link>
     </div>
-    <Link to={`/person/${getPersonId(name, lastname)}`}>
-      {name} {lastname}
-    </Link>
-  </div>
-)
+  )
+}
 
 export default props => {
-  const {pathContext: {data: person}} = props
+  const { pathContext: { data: person } } = props
   return (
     <div>
       <div className={styles.list}>
-        {person.map(({node}, index) => {
+        {person.map(({ node }, index) => {
           return <Person
             key={index}
             node={node} />
