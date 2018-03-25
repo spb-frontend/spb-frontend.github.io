3fa7a2d67548599a6a2684993a5a2b406e461837
Alexander Kurganow <me@akurganow.ru>
2018-03-25 23:59:36 +0300

    fix code style

--- src/components/person.js
+++ src/components/person.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
-import moment from 'moment'
 
 import styles from './../css/person.module.css'
 import { defaultHelmetMeta } from '../layouts/index'
