29ce3e06ea326a91c2bcebcb8488912863e802b5
Danakt <danakt@protonmail.com>
2022-02-10 09:07:04 +0300

    Init next

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hello world</title>
+      </Head>
+      <div>Hello world</div>
+    </>
+  );
+}
