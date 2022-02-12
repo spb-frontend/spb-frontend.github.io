d8fa591fdef994f6735c61d79eb30fea99f80eb1
elemeNtk0 <elemeNtk0gm@gmail.com>
2022-02-12 18:28:55 +0300

    Updated markup in About part (added Content wrapp, and removed extra styles)

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,11 +2,14 @@ import React, { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 import classNames from 'classnames';
 import { AboutItem } from '../AboutItem/AboutItem';
+import { Content } from '../Content/Content';
+
 import titleBridge from './title-bridge.svg';
 import bgTilda from './bg-tilda.svg';
 
 const Wrapper = styled.section`
-  padding: 65px 20px 30px;
+  padding-top: 65px;
+  padding-bottom: 30px;
 
   background-repeat: no-repeat;
   background-position: center top 255px;
@@ -14,12 +17,14 @@ const Wrapper = styled.section`
   background-size: contain;
 
   @media (min-width: 670px) {
-    padding: 122px 58px 60px;
+    padding-top: 122px;
+    padding-bottom: 60px;
     background-position: center top 261px;
   }
 
   @media (min-width: 1025px) {
-    padding: 158px 200px 100px;
+    padding-top: 158px;
+    padding-bottom: 100px;
     background-position: center top 242px;
   }
 `;
@@ -103,62 +108,64 @@ type Props = PropsWithChildren<{
 export const About = (props: Props) => {
   return (
     <Wrapper className=''>
-      <AboutTitle>О нас</AboutTitle>
-      <AboutDesc>
-        Мы&nbsp;регулярно проводим встречи с&nbsp;2014&nbsp;года. Обычно это
-        встречи с докладами по&nbsp;30&nbsp;минут, но&nbsp;бывают разные
-        форматы. Встречаемся обычно в пространствах в&nbsp;центре города.
-      </AboutDesc>
+      <Content>
+        <AboutTitle>О нас</AboutTitle>
+        <AboutDesc>
+          Мы&nbsp;регулярно проводим встречи с&nbsp;2014&nbsp;года. Обычно это
+          встречи с докладами по&nbsp;30&nbsp;минут, но&nbsp;бывают разные
+          форматы. Встречаемся обычно в пространствах в&nbsp;центре города.
+        </AboutDesc>
 
-      <AboutList>
-        <AboutItem type='mail'>
-          Если хотите <b>выступить с&nbsp;докладом</b>, напишите письмо на{' '}
-          <a href='mailto:hi@spb-frontend.ru' target='_blank'>
-            hi@spb-frontend.ru
-          </a>
-          .
-        </AboutItem>
-        <AboutItem type='breakfast'>
-          Ещё по четвергам теперь бывают фронтенд-завтраки в центре города.
-        </AboutItem>
-        <AboutItem type='tg'>
-          Если хотите предложить вакансию, то&nbsp;это можно сделать только в
-          телеграм-чате сообщества{' '}
-          <a href='https://t.me/spb_frontend_chat' target='_blank'>
-            SPB Frontend
-          </a>
-          .
-        </AboutItem>
-        <AboutItem type='networking'>
-          Иногда мы&nbsp;проводим дринкапы: встречи в&nbsp;барах без докладов,
-          просто чтобы пообщаться друг с&nbsp;другом, познакомиться, поговорить
-          о&nbsp;наболевшем.
-        </AboutItem>
-        <AboutItem type='social'>
-          Чтобы следить за&nbsp;новыми событиями сообщества, подписывайтесь
-          на&nbsp;
-          <a href='https://vk.com/spb_frontend' target='_blank'>
-            группу ВКонтакте
-          </a>
-          ,{' '}
-          <a href='https://twitter.com/spb_frontend' target='_blank'>
-            Твиттер
-          </a>{' '}
-          или{' '}
-          <a href='https://t.me/spb_frontend' target='_blank'>
-            Телеграм канал
-          </a>
-          .
-        </AboutItem>
-        <AboutItem type='sponsor'>
-          Если хотите предложить спонсорство или площадку (от 100 человек),
-          пишите нам на почту{' '}
-          <a href='mailto:hi@spb-frontend.ru' target='_blank'>
-            hi@spb-frontend.ru
-          </a>
-          .
-        </AboutItem>
-      </AboutList>
+        <AboutList>
+          <AboutItem type='mail'>
+            Если хотите <b>выступить с&nbsp;докладом</b>, напишите письмо на{' '}
+            <a href='mailto:hi@spb-frontend.ru' target='_blank'>
+              hi@spb-frontend.ru
+            </a>
+            .
+          </AboutItem>
+          <AboutItem type='breakfast'>
+            Ещё по четвергам теперь бывают фронтенд-завтраки в центре города.
+          </AboutItem>
+          <AboutItem type='tg'>
+            Если хотите предложить вакансию, то&nbsp;это можно сделать только в
+            телеграм-чате сообщества{' '}
+            <a href='https://t.me/spb_frontend_chat' target='_blank'>
+              SPB Frontend
+            </a>
+            .
+          </AboutItem>
+          <AboutItem type='networking'>
+            Иногда мы&nbsp;проводим дринкапы: встречи в&nbsp;барах без докладов,
+            просто чтобы пообщаться друг с&nbsp;другом, познакомиться,
+            поговорить о&nbsp;наболевшем.
+          </AboutItem>
+          <AboutItem type='social'>
+            Чтобы следить за&nbsp;новыми событиями сообщества, подписывайтесь
+            на&nbsp;
+            <a href='https://vk.com/spb_frontend' target='_blank'>
+              группу ВКонтакте
+            </a>
+            ,{' '}
+            <a href='https://twitter.com/spb_frontend' target='_blank'>
+              Твиттер
+            </a>{' '}
+            или{' '}
+            <a href='https://t.me/spb_frontend' target='_blank'>
+              Телеграм канал
+            </a>
+            .
+          </AboutItem>
+          <AboutItem type='sponsor'>
+            Если хотите предложить спонсорство или площадку (от 100 человек),
+            пишите нам на почту{' '}
+            <a href='mailto:hi@spb-frontend.ru' target='_blank'>
+              hi@spb-frontend.ru
+            </a>
+            .
+          </AboutItem>
+        </AboutList>
+      </Content>
     </Wrapper>
   );
 };
