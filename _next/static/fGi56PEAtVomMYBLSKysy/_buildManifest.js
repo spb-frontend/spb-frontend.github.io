610e06b956961f720f43f9836ed4b89a30c4f48e
danakt <danakt@users.noreply.github.com>
2023-05-28 12:58:48 +0000

    deploy: 9cec7f921e2ba6500b43288169368b4aac57c98b

--- _next/static/fGi56PEAtVomMYBLSKysy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/545f34e4-c39ab329e25540a6.js","static/chunks/500-c3b7ac1764e1c22f.js","static/chunks/pages/index-9eae4b3751a5625a.js"],"/_error":["static/chunks/pages/_error-7cc79ac3574486ac.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
