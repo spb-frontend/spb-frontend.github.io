297d2c9db1c9afbb7a82c0f15081be5521732878
Danakt <danakt@protonmail.com>
2023-05-28 19:53:01 +0300

    Preload fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,9 +18,24 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:title" content="SPB Frontend" />
         <meta property="og:site_name" content="SPB Frontend" />
         <meta property="og:url" content="https://spb-frontend.ru" />
-        <meta property="og:description" content="SPB Frontend — сообщество петербургских фронтенд-разработчиков" />
+        <meta property="og:description" content="SPB Frontend. Сообщество петербургских фронтенд-разработчиков" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/og.png" />
+        <meta property="og:image" content="/og.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="ru_RU" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="SPB Frontend" />
+        <meta name="twitter:description" content="Сообщество петербургских фронтенд-разработчиков" />
+        <meta name="twitter:url" content="https://spb-frontend.ru" />
+        <meta name="twitter:image" content="/og.jpg" />
+
+        {/* Fonts preload */}
+        <link rel="preload" href="/fonts/Formular-BoldItalic.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Formular-Bold.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Formular-Light.woff2" as="font" type="font/woff2" crossOrigin="" />
+        <link rel="preload" href="/fonts/Formular-LightItalic.woff2" as="font" type="font/woff2" crossOrigin="" />
 
         {/* Favicon */}
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
