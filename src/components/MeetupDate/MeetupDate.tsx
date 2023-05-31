a9761633b5ab8820aaf8543e2df00ba7744e6578
Danakt <danakt@protonmail.com>
2023-06-01 01:09:31 +0300

    Past event date fix

--- src/components/MeetupDate/MeetupDate.tsx
+++ src/components/MeetupDate/MeetupDate.tsx
@@ -59,7 +59,8 @@ export const MeetupDate = (props: Props) => {
         {props.isPastEvent ? (
           <>
             <IoIosAlert className={styles.pastEventIcon} size={25} />
-            Завершилось {dateRender}
+            Завершилось&nbsp;
+            {dateRender}
           </>
         ) : (
           <>
